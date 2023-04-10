92ae579947b59741dc090e91c9761ce1b3f3d79c
myprogram-ishi <m_ishikawa@dpt-inc.co.jp>
2023-04-10 14:45:37 +0900

    サンプルプログラムを改造

--- folderTreeControl.py
+++ folderTreeControl.py
@@ -19,16 +19,16 @@ def getFolderTree(path, layer=0, is_last=False, indent_current=data.indent_tree)
     if not pathlib.Path(path).is_absolute():
         path = str(pathlib.Path(path).resolve())
 
-    # カレントディレクトリの表示
-    current = path.split('/')[::-1][0]
+    current = path.split('\\')[::-1][0]
     if layer == 0:
+        # カレントディレクトリの表示
         print('<'+current+'>')
     else:
         branch = '└' if is_last else '├'
-        print('{indent}{branch}<{dirname}>'.format(indent=indent_current, branch=branch, dirname=current))
+        print('{indent}{branch}{dirname}'.format(indent=indent_current, branch=branch, dirname=current))
 
     # 下の階層のパスを取得
-    paths = [p for p in glob.glob(path+'/*') if os.path.isdir(p) or os.path.isfile(p)]
+    paths = [p for p in glob.glob(path+'/*') if os.path.isdir(p)]
     def is_last_path(i):
         return i == len(paths)-1
 
@@ -37,11 +37,8 @@ def getFolderTree(path, layer=0, is_last=False, indent_current=data.indent_tree)
 
         indent_lower = indent_current
         if layer != 0:
-            indent_lower += '　　' if is_last else '│　'
+            indent_lower += data.indent_tree if is_last else '│　'
 
-        if os.path.isfile(p):
-            branch = '└' if is_last_path(i) else '├'
-            print('{indent}{branch}{filename}'.format(indent=indent_lower, branch=branch, filename=p.split('/')[::-1][0]))
         if os.path.isdir(p):
             getFolderTree(p, layer=layer+1, is_last=is_last_path(i), indent_current=indent_lower)
 
