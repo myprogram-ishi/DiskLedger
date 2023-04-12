06c8232717c88c6fcc3761976a26a1fe78de8023
myprogram-ishi <m_ishikawa@dpt-inc.co.jp>
2023-04-12 13:57:52 +0900

    微修正。（直接定義されていた文字列を変数化など）

--- data.py
+++ data.py
@@ -3,7 +3,14 @@
 #   文字列
 ###############################
 
+#エクセルシート名
+shtName_main = 'メイン'
+shtName_base = '基準'
+shtName_pyDbgLog = 'python_log'
+
 xlInterface = 'Module_interfacePython'
+
+getEndRowCout = 'getEndOfRowCount'
 
 strFunc = "[func]"
 
