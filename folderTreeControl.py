1d5130c4b5594fd1354f18c40d7925465978442f
unknown <m_ishikawa@dpt-inc.co.jp>
2023-04-27 11:22:17 +0900

    関数割変更。これにより、下記関数の新規追加
    def expandFolderTree_for_OneBranch(TipBranch, branchIndex):

--- folderTreeControl.py
+++ folderTreeControl.py
@@ -37,88 +37,95 @@ def expandFolderTree_from_BranchTip(root, col):
     rowDownCnt = len(data.lst_branch)   #エクセルシート↑の行番号なので、１から配列要素お個数分。
     lst_end = rowDownCnt - 1            #配列絵インデックスの末尾
     branchIndex = lst_end
-    before_ret = 0
-    before_mark = 0
-    before_branchMarkPos = -1   #府数設定は、初期状態を意味する
 
     while branchIndex > 0:
-        #ブランチ名
-        work_expandFullPath = data.lst_branch[branchIndex]
-        dummyRet, dummyMark, topPos = getPostionOfHoraizonalBar(work_expandFullPath)
 
-        expandFullPath = work_expandFullPath[topPos + 1:]
-        work_branchIndex = branchIndex  #フォルダの一番下の階層（初期値）
-        before_branchMarkPos = -1       #負数で、初期値を表す
-
-        #ブランチにパスを付加していく
-        while work_branchIndex >= 0:
-
-            work_lst_branch = data.lst_branch[work_branchIndex]
-            curr_ret, curr_mark, curr_branchMarkPos = isIncludeBranchMark(work_lst_branch)
-
-            #フォルダの一番下の階層
-            work_expandFullPath = expandFullPath
-
-            r_ret, r_mark, topPos_expnd = isIncludeBranchMark(work_expandFullPath)
-
-            #topPos_lstbrnch = work_lst_branch.find('─')
-            dummyRet, dummyMark, topPos_lstbrnch = getPostionOfHoraizonalBar(work_expandFullPath)
-
-            if topPos_lstbrnch < 0:
-                topPos_lstbrnch = work_lst_branch.find('└')
-
-            if before_branchMarkPos < 0:
-                #最初の１回目
-                expandFullPath = work_expandFullPath[topPos_expnd+1:]
-                expandFullPath = expandFullPath + '>' + str(work_branchIndex) + '>'
-                before_branchMarkPos = curr_branchMarkPos
-
-            elif curr_branchMarkPos == before_branchMarkPos:
-                currentBranch = data.lst_branch[branchIndex]
-                topPos_lstbrnch = currentBranch.find('├')
-                if topPos_lstbrnch >= 0:
-                    #expandFullPath = 'branchIndex = ' + str(branchIndex) + 'work_branchIndex = ' + str(work_branchIndex)
-                    expandFullPath = currentBranch[topPos_lstbrnch+1:]
-                else:
-                    topPos_lstbrnch = currentBranch.find('└')
-                    if topPos_lstbrnch >= 0:
-                        expandFullPath =  currentBranch[topPos_lstbrnch+1:]
-                    else:
-                        expandFullPath = '???' + work_lst_branch
-
-                result, dummyMark, topPos = getPostionOfHoraizonalBar(expandFullPath)
-                if result == True:
-                    expandFullPath = expandFullPath[topPos+1:]
-
-                expandFullPath = expandFullPath + '{' + str(work_branchIndex) + '}'
-
-            elif curr_branchMarkPos < before_branchMarkPos:
-                upperBranch = work_lst_branch[topPos_lstbrnch + 1:]
-
-                #末尾に\がついているかいないかで処理を分ける
-                if upperBranch[-1] == '\\':
-                    expandFullPath = upperBranch + work_expandFullPath[topPos_expnd + 1:]
-                else:
-                    expandFullPath = upperBranch + '\\' + work_expandFullPath[topPos_expnd+1:]
-
-                expandFullPath = expandFullPath + '[' + str(work_branchIndex) + ']'
-
-                result, dummyMark, topPos = getPostionOfHoraizonalBar(expandFullPath)
-                if result == True:
-                    expandFullPath = expandFullPath[topPos+1:]
-
-                before_branchMarkPos = curr_branchMarkPos
-
-            work_branchIndex = work_branchIndex - 1
+        ret_expandFullPath = expandFolderTree_for_OneBranch(data.lst_branch[branchIndex], branchIndex)
 
         #生成したフルパスを追加
-        data.lst_expandFolderTreeTarget.append(expandFullPath)
+        data.lst_expandFolderTreeTarget.append(ret_expandFullPath)
         #inerfaceExcel.excelIO_UDF_appendDataToLasRow(data.shtName_Expand, 1, expandFullPath)
-        inerfaceExcel.excelIO_UDF_outputdebugLog(srcExcel=None, shtName=data.shtName_Expand, row=rowDownCnt, col=col, outval=expandFullPath)
+        inerfaceExcel.excelIO_UDF_outputdebugLog(srcExcel=None, shtName=data.shtName_Expand, row=rowDownCnt, col=col, outval=ret_expandFullPath)
         rowDownCnt = rowDownCnt - 1
 
         #次のブラチのフルパス作成のための変数設定
-        branchIndex = branchIndex -1
+        branchIndex = branchIndex - 1
+
+########################################################
+#   フォルダ一つ分のフルパス生成
+########################################################
+def expandFolderTree_for_OneBranch(TipBranch, branchIndex):
+    # ブランチ名
+
+    work_branch = TipBranch
+    work_branchIndex = branchIndex
+    dummyRet, dummyMark, topPos = getPostionOfHoraizonalBar(work_branch)
+
+    expandFullPath = work_branch[topPos + 1:]
+    before_branchMarkPos = -1  # 負数で、初期値を表す
+
+    # ブランチにパスを付加していく
+    while work_branchIndex >= 0:
+
+        work_lst_branch = data.lst_branch[work_branchIndex]
+        curr_ret, curr_mark, curr_branchMarkPos = isIncludeBranchMark(work_lst_branch)
+
+        # topPos_lstbrnch = work_lst_branch.find('─')
+        dummyRet, dummyMark, topPos = getPostionOfHoraizonalBar(work_lst_branch)
+        if topPos > 0:
+            work_lst_branch = work_lst_branch[topPos + 1:]
+
+        if topPos < 0:
+            topPos = work_lst_branch.find('└')
+
+        if before_branchMarkPos < 0:
+            # 最初の１回目
+            expandFullPath = work_lst_branch[curr_branchMarkPos + 1:]
+            expandFullPath = expandFullPath + '<' + str(work_branchIndex) + '>' + '[' + str(before_branchMarkPos) + ']'
+            before_branchMarkPos = curr_branchMarkPos
+
+        elif curr_branchMarkPos == before_branchMarkPos:
+            currentBranch = data.lst_branch[branchIndex]
+            topPos_lstbrnch = currentBranch.find('├')
+            if topPos_lstbrnch >= 0:
+                # expandFullPath = 'branchIndex = ' + str(branchIndex) + 'work_branchIndex = ' + str(work_branchIndex)
+                expandFullPath = currentBranch[topPos_lstbrnch + 1:]
+            else:
+                topPos_lstbrnch = currentBranch.find('└')
+                if topPos_lstbrnch >= 0:
+                    expandFullPath = currentBranch[topPos_lstbrnch + 1:]
+                else:
+                    expandFullPath = '???' + work_lst_branch
+
+            result, dummyMark, topPos = getPostionOfHoraizonalBar(expandFullPath)
+            if result == True:
+                expandFullPath = expandFullPath[topPos + 1:]
+
+            expandFullPath = expandFullPath + '{' + str(work_branchIndex) + '}'
+
+        elif curr_branchMarkPos < before_branchMarkPos:
+            upperBranch = work_lst_branch[curr_branchMarkPos + 1:]
+            #upperBranch = work_lst_branch
+
+            if len(upperBranch) <= 0:
+                break
+            if upperBranch[-1] == '\\':
+                # 末尾に\がついているかいないかで処理を分ける
+                expandFullPath = upperBranch + expandFullPath
+            else:
+                expandFullPath = upperBranch + '\\' + expandFullPath
+
+            expandFullPath = expandFullPath + '[' + str(work_branchIndex) + ']'
+
+            result, dummyMark, topPos = getPostionOfHoraizonalBar(expandFullPath)
+            if result == True:
+                expandFullPath = expandFullPath[topPos + 1:]
+
+            before_branchMarkPos = curr_branchMarkPos
+
+        work_branchIndex = work_branchIndex - 1
+
+    return expandFullPath
 
 ########################################################
 #   フォルダツリーのトップから探す
